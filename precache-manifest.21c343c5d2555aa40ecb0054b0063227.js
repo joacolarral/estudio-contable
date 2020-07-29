029773daa202870df922f7f75ee60e7f52a43d9b
joacolarral <joacolarral@gmail.com>
2020-07-29 15:57:51 -0300

    Updates

---
+++ precache-manifest.21c343c5d2555aa40ecb0054b0063227.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e298e396839f9e341a8578fbf011d2",
+    "url": "/estudio-contable/index.html"
+  },
+  {
+    "revision": "0126a1953481920a6dbb",
+    "url": "/estudio-contable/static/css/2.bca29b35.chunk.css"
+  },
+  {
+    "revision": "21bd4ac9ca4187d1c60e",
+    "url": "/estudio-contable/static/css/main.99314b54.chunk.css"
+  },
+  {
+    "revision": "0126a1953481920a6dbb",
+    "url": "/estudio-contable/static/js/2.1b49ea94.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/estudio-contable/static/js/2.1b49ea94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21bd4ac9ca4187d1c60e",
+    "url": "/estudio-contable/static/js/main.a9f3c511.chunk.js"
+  },
+  {
+    "revision": "4323a248e28ea6ec4f82",
+    "url": "/estudio-contable/static/js/runtime-main.c54de381.js"
+  },
+  {
+    "revision": "9ea537a0cc5c2eb9fd96b1abc8713ea4",
+    "url": "/estudio-contable/static/media/contactUs.9ea537a0.png"
+  },
+  {
+    "revision": "598642bc2ad2ab743a49dd1e617df6fb",
+    "url": "/estudio-contable/static/media/rsz_contabilidad.598642bc.jpg"
+  },
+  {
+    "revision": "23ad3f3fbab8c2c16947521e1706878a",
+    "url": "/estudio-contable/static/media/rsz_impuestos.23ad3f3f.jpg"
+  },
+  {
+    "revision": "f27b9d5650deef0785030a7cf892a53c",
+    "url": "/estudio-contable/static/media/rsz_laboral.f27b9d56.jpg"
+  },
+  {
+    "revision": "89d21e1d1134f3fa9d62ec994c7451ca",
+    "url": "/estudio-contable/static/media/rsz_sociedades.89d21e1d.jpg"
+  }
+]);
